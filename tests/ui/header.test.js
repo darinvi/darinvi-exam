0379645d9a1c6eb72520c955349a6101ac0e9aad
Darin Ivanov <d.v.ivanov98@gmail.com>
2023-12-16 15:22:51 +0200

    Fix broken URL from previous commit

--- tests/ui/header.test.js
+++ tests/ui/header.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check header', async ({ page }) => {
-    await page.goto('https://darinvi-exam.onrender.com/'); 
+    await page.goto('https://darinvi-exam.onrender.com'); 
     const homeLink = await page.$('a[href="/"]');
     const text = await homeLink.textContent();
     expect(text).toBe('Home');
